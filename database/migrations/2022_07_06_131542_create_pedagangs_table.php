f20895d8764016def49e53e22f270b700f13ee7b
raffka90 <108121608+raffka90@users.noreply.github.com>
2022-07-07 15:00:27 +0700

    migration, Register dan login

--- database/migrations/2022_07_06_131542_create_pedagangs_table.php
+++ database/migrations/2022_07_06_131542_create_pedagangs_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pedagangs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses',['admin','user']);
-            $table->rememberToken();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('nohp');
+            $table->bigInteger('lapaks_id');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pedagangs');
     }
 };
